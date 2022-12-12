8b39374e11440781a8a2aeab717444d3bc00da8d
Luka Govedic <luka.govedic@gmail.com>
2022-12-12 02:17:16 -0500

    Detector refactoring

--- src/afqlite/video/detector.py
+++ src/afqlite/video/detector.py
@@ -11,9 +11,7 @@ DetectionTuple = tuple[float, float, float, float, float, int, int, str]
 class Detector:
     
     def __init__(self, 
-                 model_path, 
-                 vid_data_path, 
-                 vid_data_ls,
+                 model_path,
                  name):
         """
         model_path (str): path to model
@@ -22,9 +20,7 @@ class Detector:
         name (str): name chosen by user to identify this detector
         """
         self.model_path = model_path
-        self.vid_data_path = vid_data_path #this might not be an argument but a variable global to proj
-        self.vid_data_ls = vid_data_ls
-        self.name = name        
+        self.name = name
         self.model = None
         self.classifer_hash = None #?
         
@@ -46,11 +42,11 @@ class Detector:
         print(f"    {bitwidth}-bit k-means quantized model has size={quantized_model_size/MiB:.2f} MiB")
         #prune(self.model, 0.3)
         
-    def detect(self, timestamp, classes, confidence):
+    def detect(self, timestamp, classes, confidence, vid_data_path):
         self.model.conf = confidence
         self.model.classes = classes
         
-        vid_loader = VideoLoader(self.vid_data_path, 'myFrame')
+        vid_loader = VideoLoader(vid_data_path, 'myFrame')
         img = vid_loader.getSingleFrame(timestamp)
         return self.getResultFromImg(img, timestamp)
         
@@ -70,11 +66,12 @@ class Detector:
                               frame_end,
                               frame_jump,
                               classes,
-                              confidence):
+                              confidence,
+                              vid_data_path):
         all_values = []
         
         for i in range(frame_start, frame_end, frame_jump):
-            res = self.detect(i, classes, confidence)
+            res = self.detect(i, classes, confidence, vid_data_path)
             all_values.extend(res)
         df_to_ret = pandas.DataFrame(all_values)
         df_to_ret.columns = ["xmin","ymin","xmax","ymax", "confidence","class","name","timestamp"]
@@ -83,11 +80,11 @@ class Detector:
 if __name__ == "__main__":
     
     #dtc = Detector('ultralytics/yolov5', 'pexels-blue-bird-7189538.mp4', None, 'yolov5s')
-    dtc = Detector('ultralytics/yolov5', 'pexels-blue-bird-7189538.mp4', None, 'yolov5s')
+    dtc = Detector('ultralytics/yolov5', 'yolov5s')
 
     for i in range(0, 36, 12):
         base = "runs"
-        dtc.detect(i, [0, 16], 0.7)
+        dtc.detect(i, [0, 16], 0.7, 'pexels-blue-bird-7189538.mp4')
         
     print('made it here')
 
